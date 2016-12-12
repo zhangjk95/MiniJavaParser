9354ce211685fd0562ceebb488e6cc98c889624c
zhangjk95 <zjk_261011@163.com>
2016-12-12 14:46:18 +0800

    打包

--- run.bat
+++ run.bat
@@ -1,3 +1,1 @@
-cd src
-java -cp ".;C:\Library\ANTLR\antlr-4.5.3-complete.jar" org.antlr.v4.Tool -visitor MiniJava.g4
-cd ..
+java -cp ".\out\artifacts\MiniJavaParser_jar\MiniJavaParser.jar" Main test.txt
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp "./out/artifacts/MiniJavaParser_jar/MiniJavaParser.jar" Main test.txt
